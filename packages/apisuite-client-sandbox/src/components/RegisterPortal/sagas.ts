cdff3eda6862e2dbad3138cd68105055f8ca7eb4
Joao F E <joaoeirinh@gmail.com>
2020-01-17 11:17:09 +0000

    feat(RegisterPortal): added redirect to register

--- packages/apisuite-client-sandbox/src/components/RegisterPortal/sagas.ts
+++ packages/apisuite-client-sandbox/src/components/RegisterPortal/sagas.ts
@@ -3,18 +3,30 @@
  */
 
 import { REGISTER_USER } from './ducks'
-import { takeLatest, call } from 'redux-saga/effects'
+import { takeLatest, call, put } from 'redux-saga/effects'
 import { RegisterAction } from './types'
 import { API_URL, SIGNUP_PORT, SIGNUP_ROUTE } from 'constants/endpoints'
 import request from 'util/request'
+import { authActions } from 'containers/Auth/ducks'
+import requireImage from 'util/requireImage'
 
 export function * registerUser (action: RegisterAction) {
-  const requestUrl = `${API_URL}:${SIGNUP_PORT}${SIGNUP_ROUTE}`
-  yield call(request, requestUrl, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(action.userData),
-  })
+  try {
+    const requestUrl = `${API_URL}:${SIGNUP_PORT}${SIGNUP_ROUTE}`
+
+    yield call(request, requestUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(action.userData),
+    })
+
+    yield put(authActions.loginSuccess({
+      token: 'mock_token',
+      user: { fName: 'Quentin', lName: 'Felice', avatar: requireImage('goncalo-avatar.jpg') },
+    }))
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 function * rootSaga () {
