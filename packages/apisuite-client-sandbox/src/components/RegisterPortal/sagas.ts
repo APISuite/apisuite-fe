04b5254d38260223279a310f9548d46844849722
Joao F E <joaoeirinh@gmail.com>
2020-01-22 10:01:58 +0000

    feat(RegisterPortal): Change to url

--- packages/apisuite-client-sandbox/src/components/RegisterPortal/sagas.ts
+++ packages/apisuite-client-sandbox/src/components/RegisterPortal/sagas.ts
@@ -5,14 +5,14 @@
 import { REGISTER_USER } from './ducks'
 import { takeLatest, call, put } from 'redux-saga/effects'
 import { RegisterAction } from './types'
-import { API_URL, SIGNUP_PORT, SIGNUP_ROUTE } from 'constants/endpoints'
+import { API_URL, SIGNUP_PORT } from 'constants/endpoints'
 import request from 'util/request'
 import { authActions } from 'containers/Auth/ducks'
 import requireImage from 'util/requireImage'
 
 export function * registerUser (action: RegisterAction) {
   try {
-    const requestUrl = `${API_URL}:${SIGNUP_PORT}${SIGNUP_ROUTE}`
+    const requestUrl = `${API_URL}:${SIGNUP_PORT}/users/register`
 
     yield call(request, requestUrl, {
       method: 'POST',
