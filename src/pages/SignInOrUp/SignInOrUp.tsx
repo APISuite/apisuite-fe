aec1e716f0c169e6e6d8afd35f87c4535b4c74a5
Bruno Morgado <mstrk512@gmail.com>
2021-06-24 10:45:38 +0100

    fix(SignInOrUp): views not centered

--- src/pages/SignInOrUp/SignInOrUp.tsx
+++ src/pages/SignInOrUp/SignInOrUp.tsx
@@ -131,6 +131,8 @@ export const SignInOrUp: React.FC = () => {
         component={Box}
         container
         direction="column"
+        pt={view === "invitation" || !sso.length ? spacing(2.5) : 0}
+        pb={view === "invitation" || !sso.length ? spacing(1) : 0}
         px={6}
         maxWidth={breakpoints.values.sm}
         margin="auto"
