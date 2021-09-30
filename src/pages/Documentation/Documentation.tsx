b618c28e5741e7af949d59c29aa8e8ff5e59ba9e
a31859 <a31859@users.noreply.github.com>
2021-09-30 13:01:03 +0100

    fix(docs): update documentation to use markdown component

--- src/pages/Documentation/Documentation.tsx
+++ src/pages/Documentation/Documentation.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Grid } from "@apisuite/fe-base";
+import { Grid, useTranslation } from "@apisuite/fe-base";
 import { Markdown } from "components/Markdown";
 import { PageContainer } from "components/PageContainer";
 
 export const Documentation: React.FC = () => {
+
+  const [t] = useTranslation();
+
   return (
     <PageContainer>
       <Grid alignContent="center" alignItems="center" container justify="center">
         <Grid item md={12}>
-          <Markdown page="documentation" />
+          <Markdown page="documentation" defaultValue={t("dashboardTab.documentation.default")} />
         </Grid>
       </Grid>
     </PageContainer>
