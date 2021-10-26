11a2f14b0c43490cb9c3f29f013d20bc1e8280f9
Pedro Carvalho <pedro@cloudoki.com>
2021-10-26 15:39:26 +0100

    feat: Added query parameter templates (to be replaced in the future)

--- src/store/subscriptions/sagas.ts
+++ src/store/subscriptions/sagas.ts
@@ -5,9 +5,39 @@ import request from "util/request";
 import { handleSessionExpire } from "store/auth/actions/expiredSession";
 import { getAPIsError, getAPIsSuccess, GET_APIS } from "./actions/getAPIs";
 
+/**
+ * Takes a list of items and builds a string usable as a URL query string.
+ * Ex.: buildQueryParameters([1,2], 'user') => 'user=1&user=2'
+ * */
+function buildQueryParameters(items: string[], paramName: string): string {
+  let params = "";
+  for (let i = 0; i < items.length; i++) {
+    if (i === 0) {
+      params = `${paramName}=${items[i]}`;
+      continue;
+    }
+    params += `&${paramName}=${items[i]}`;
+  }
+  return params;
+}
+
 function * getAPIsSaga () {
   try {
-    const getAPIsUrl = `${API_URL}/apis`;
+    const pagination = "page=1&pageSize=100";
+    let getAPIsUrl = `${API_URL}/apis?${pagination}`;
+
+    const typeParams = buildQueryParameters(["production, documentation"], "type");
+    const sortParams = buildQueryParameters(["published"], "sort_by");
+    const orderParams = buildQueryParameters(["asc"], "order");
+    const name = "";
+    const search = "";
+
+    const params = [typeParams, sortParams, orderParams, name, search]
+      .filter((p) => p.length)
+      .join("&");
+
+    getAPIsUrl = params.length ? `${getAPIsUrl}&${params}` : getAPIsUrl;
+
     const response: ApisResponse = yield call(request, {
       url: getAPIsUrl,
       method: "GET",
