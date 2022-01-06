1d1880df218f2abd40e8302a2916089a3fae2c64
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2022-01-06 14:21:46 +0000

    feat: cloud url & domains update

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -4,31 +4,27 @@ const { hostname } = window.location;
 
 export const IS_CLOUD = hostname.indexOf(".cloud.apisuite.io") >= 0;
 
+function buildCloudServiceUrl (service: string) {
+  const client = hostname.substring(0, hostname.indexOf("."));
+  const apiHostname = hostname.replace(client, `${service}.${client}`);
+  return `https://${apiHostname}`;
+}
+
 /**
  * For when running in the cloud environment.
  * Given the current Portal hostname, get the corresponding domain for another
  * service running in a given subdomain prefix.
- * Ex: ${client}.cloud.apisuite.io -> ${client}-${subdomainSuffx}.cloud.apisuite.io
+ * Ex: ${client}.cloud.apisuite.io -> ${service}.${client}.cloud.apisuite.io
  *
- * @param subdomainSuffix
+ * @param service
  */
-export function getCloudUrlForSubdomainSuffix (subdomainSuffix: string) {
-  if (IS_CLOUD) {
-    const apiHostname = hostname.replace(".", `-${subdomainSuffix}.`);
-    return `https://${apiHostname}`;
-  }
-
+export function getCloudUrlForSubdomainSuffix (service: string) {
+  if (IS_CLOUD) return buildCloudServiceUrl(service);
   return null;
 }
 
 function getApiUrl () {
-  if (IS_CLOUD) {
-    // Transform the Portal's hostname into the API's hostname
-    // Ex: ${client}.cloud.apisuite.io -> ${client}-apisuite-api.cloud.apisuite.io
-    const apiHostname = hostname.replace(".", "-apisuite-api.");
-    return `https://${apiHostname}`;
-  }
-
+  if (IS_CLOUD) return buildCloudServiceUrl("api");
   return process.env.API_URL || "";
 }
 
