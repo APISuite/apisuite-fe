78941990f2547cb22ce11962aef75dc22b535246
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2022-01-12 18:42:29 +0000

    fix: fixed cloud backend url build function

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,13 +1,12 @@
-/** Endpoints constants */
-
 const { hostname } = window.location;
 
 export const IS_CLOUD = hostname.indexOf(".cloud.apisuite.io") >= 0;
 
 export function buildCloudBackendUrl (service = "") {
   const client = hostname.substring(0, hostname.indexOf("."));
-  const apiHostname = hostname.replace(client, `${client}-api`);
-  const url = new URL(service, `https://${apiHostname}`);
+  const apiHostname = 'https://' + hostname.replace(client, `${client}-api`);
+  if (!service.length) return apiHostname;
+  const url = new URL(service, apiHostname);
   return url.toString();
 }
 
