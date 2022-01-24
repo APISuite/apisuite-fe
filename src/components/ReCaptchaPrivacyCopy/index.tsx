6bb2b293ad0989305db85d26824f32fbb1ebe36d
Bruno Morgado <mstrk512@gmail.com>
2022-01-24 10:25:29 +0000

    perf: add reCaptcha to sensible endpoints (#543)
    * perf(updatePasswordRequestSaga): adds reCaptcha
    
    * perf(forgotPasswordSaga): adds reCaptcha
    
    * perf: adds reCaptcha in login and invitation
    
    * refactor: minor change to recaptcha actions
    
    * perf(SignUpForm): redone and reCaptcha added
    
    * chore: clean dead code
    
    * refactor: removes local pt-PT translations
    
    * feat(ReCaptchaPrivacyCopy): adds common reCaptcha copy
    
    * refactor(SignInForm): adds recaptcha copy
    
    * refactor(PasswordRecovery): adds recaptcha copy
    
    * refactor: notification error if calling loginError
    
    * refactor(Security): adds recaptcha copy
    
    * chore(generate_envfile): update environment variables
    
    * fix(SignUpForm): wrong organization field name

---
+++ src/components/ReCaptchaPrivacyCopy/index.tsx
@@ -0,0 +1,1 @@
+export { ReCaptchaPrivacyCopy } from "./ReCaptchaPrivacyCopy";
--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,133 +1,195 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Redirect } from "react-router-dom";
+import React, { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Trans, Typography, useTheme, useTranslation } from "@apisuite/fe-base";
-
-import StepsProgress from "components/StepsProgress";
-
-import { ProfileDetailsForm } from "./ProfileDetailsForm";
-import { OrganisationDetailsForm } from "./OrganisationDetailsForm";
-import { SecurityDetailsForm } from "./SecurityDetailsForm";
-
-import { signUpFormSelector } from "./selector";
-import { submitSignUpCredentials } from "store/auth/actions/submitSignUpCredentials";
-import { submitSignUpOrganisation } from "store/auth/actions/submitSignUpOrganisation";
+import update from "immutability-helper";
+import { Box, Button, CircularProgress, IconButton, InputAdornment, TextField, TextFieldProps, Trans, Typography, useTheme, useTranslation } from "@apisuite/fe-base";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import { notEmpty, isValidEmail, isValidPass } from "util/forms";
 import { submitSignUpDetails } from "store/auth/actions/submitSignUpDetails";
 import Link from "components/Link";
+import { signUpFormSelector } from "./selector";
+import { ReCaptchaPrivacyCopy } from "components/ReCaptchaPrivacyCopy";
 
 export const SignUpForm: React.FC = () => {
   const dispatch = useDispatch();
   const [t] = useTranslation();
   const { palette } = useTheme();
-  const { signUpError, isSignUpWorking } = useSelector(signUpFormSelector);
-  const mounted = useRef(false);
-  const submittedStep = useRef(0);
-  const confirmationName = useRef("");
-  const [step, setStep] = useState(1);
+  const { isSignUpWorking } = useSelector(signUpFormSelector);
 
-  useEffect(() => {
-    if (mounted.current && !isSignUpWorking && !signUpError) {
-      setStep((s) => Math.min(4, s + 1));
-    }
+  // Form changes logic
+  const [showPassword, setShowPassword] = useState(false);
+  const [state, setState] = useState({
+    data: {
+      name: "",
+      organization: "",
+      email: "",
+      password: "",
+    },
+    errors: {
+      email: "",
+      name: "",
+      password: "",
+    },
+  });
 
-    mounted.current = true;
-  }, [isSignUpWorking, signUpError]);
-
-  function nextStep (firstInput: string, secondInput?: string) {
-    switch (step) {
-      case 1: {
-        confirmationName.current = firstInput;
-        dispatch(submitSignUpCredentials({ details: { name: firstInput, email: secondInput! } }));
-        submittedStep.current = 1;
-        break;
-      }
-
-      case 2: {
-        if (firstInput) {
-          dispatch(submitSignUpOrganisation({ details: { orgName: firstInput, website: secondInput! } }));
-          submittedStep.current = 2;
-        } else {
-          setStep(3);
+  const handleInputChanges: TextFieldProps["onChange"] = useCallback(({ target }) => {
+    setState((s) => {
+      switch (target.name) {
+        case "name": {
+          return update(s, {
+            data: { name: { $set: target.value } },
+            errors: { name: { $set: notEmpty(target.value) ? "" : t("signUpForm.warnings.profileName") } },
+          });
         }
 
-        break;
-      }
+        case "organization": {
+          return update(s, {
+            data: { organization: { $set: target.value } },
+          });
+        }
 
-      case 3: {
-        dispatch(submitSignUpDetails({ details: { password: firstInput } }));
-        submittedStep.current = 3;
-        break;
-      }
+        case "email": {
+          return update(s, {
+            data: { email: { $set: target.value } },
+            errors: {
+              email: {
+                $set: isValidEmail(target.value) ? "" : t("signUpForm.warnings.email"),
+              },
+            },
+          });
+        }
 
-      default:
-        break;
+        case "password": {
+          return update(s, {
+            data: { password: { $set: target.value } },
+            errors: {
+              password: { $set: isValidPass(target.value) ? "" : t("signUpForm.warnings.password") },
+            },
+          });
+        }
+
+        default:
+          return s;
+      }
+    });
+  }, [t]);
+
+  const handleSubmit = useCallback(() => {
+    // only dispatch the action if the errors are not empty
+    if (!Object.values(state.errors).some(Boolean)) {
+      dispatch(submitSignUpDetails({
+        user: {
+          name: state.data.name,
+          email: state.data.email,
+          password: state.data.password,
+        },
+        organization: state.data.organization,
+      }));
     }
-  }
-
-  function prevStep () {
-    setStep((s) => Math.max(1, s - 1));
-  }
-
-  const steps = {
-    1: t("signUpForm.steps.profileDetails"),
-    2: t("signUpForm.steps.organisationDetails"),
-    3: t("signUpForm.steps.securityDetails"),
-  };
-
-  const signUpFormStep = (fromStep: number) => {
-    switch (fromStep) {
-      case 1:
-        return (
-          <ProfileDetailsForm
-            key='profileDetailsForm'
-            next={nextStep}
-            back={prevStep}
-            error={submittedStep.current === 1 && signUpError ? signUpError : ""}
-          />
-        );
-
-      case 2:
-        return (
-          <OrganisationDetailsForm
-            key='organisationDetailsForm'
-            next={nextStep}
-            back={prevStep}
-            error={submittedStep.current === 2 && signUpError ? signUpError : ""}
-          />
-        );
-
-      case 3:
-        return (
-          <SecurityDetailsForm
-            key='securityDetailsForm'
-            next={nextStep}
-            back={prevStep}
-            error={submittedStep.current === 3 && signUpError ? signUpError : ""}
-          />
-        );
-
-      default: {
-        return (
-          <Redirect
-            key='redirectToAccountConfirmation'
-            to={`/confirmation/${confirmationName.current}`}
-          />
-        );
-      }
-    }
-  };
+  }, [dispatch, state.data, state.errors]);
 
   return (
-    <>
-      <StepsProgress
-        currentStep={step}
-        steps={steps}
-      />
+    <Box>
+      <Box mt={3}>
+        <TextField
+          id='profileNameField'
+          type='text'
+          variant='outlined'
+          margin='dense'
+          name='name'
+          label={t("signUpForm.fieldLabels.profileName")}
+          value={state.data.name}
+          autoFocus
+          fullWidth
+          error={!!state.errors.name}
+          helperText={state.errors.name}
+          onChange={handleInputChanges}
+        />
+      </Box>
 
-      {signUpFormStep(step)}
+      <Box mt={2}>
+        <TextField
+          id='orgNameField'
+          variant='outlined'
+          margin='dense'
+          type='text'
+          name='organization'
+          label={t("signUpForm.fieldLabels.orgName")}
+          value={state.data.organization}
+          fullWidth
+          onChange={handleInputChanges}
+        />
+      </Box>
 
-      <Box py={3} clone>
-        <Typography variant="caption" align="center">
+      <Box mt={2}>
+        <TextField
+          id='emailField'
+          type='email'
+          variant='outlined'
+          margin='dense'
+          label={t("signUpForm.fieldLabels.profileEmail")}
+          name='email'
+          value={state.data.email}
+          placeholder=''
+          fullWidth
+          error={!!state.errors.email}
+          helperText={state.errors.email}
+          onChange={handleInputChanges}
+        />
+      </Box>
+
+      <Box mt={2}>
+        <TextField
+          id='passwordField'
+          type={showPassword ? "text" : "password"}
+          variant='outlined'
+          margin='dense'
+          label={t("signUpForm.fieldLabels.password")}
+          name='password'
+          value={state.data.password}
+          fullWidth
+          error={!!state.errors.password.length}
+          helperText={state.errors.password}
+          InputProps={{
+            endAdornment:
+              <InputAdornment position='end'>
+                <IconButton
+                  edge='end'
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>,
+          }}
+          onChange={handleInputChanges}
+        />
+      </Box>
+
+      <Box pt={5}>
+        <ReCaptchaPrivacyCopy />
+      </Box>
+
+      <Box mt={1.5}>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          disableElevation
+          fullWidth
+          onClick={handleSubmit}
+          disabled={isSignUpWorking || Object.values(state.errors).some(Boolean)}
+          endIcon={isSignUpWorking && <CircularProgress color="inherit" size={25} />}
+        >
+          {t("signInOrUpView.options.signUp")}
+        </Button>
+      </Box>
+
+      <Box py={3} width="100%" display="flex" justifyContent="center">
+        <Typography
+          variant="caption"
+          align="center"
+          color="textSecondary"
+        >
           <Trans i18nKey="signUpForm.privacyPolicyDisclaimer">
             {[
               <Link
@@ -135,12 +197,12 @@ export const SignUpForm: React.FC = () => {
                 to="https://cloudoki.atlassian.net/wiki/spaces/APIEC/pages/760938500/Privacy+Policy"
                 rel='noopener noreferrer'
                 target='_blank'
-                style={{ color: palette.text.hint }}
+                style={{ color: palette.info.main }}
               />,
             ]}
           </Trans>
         </Typography>
       </Box>
-    </>
+    </Box>
   );
 };
