588f9b873f174d2ff5af5e380978948fb289a31c
Pedro Carvalho <pedro@cloudoki.com>
2022-02-17 10:10:42 +0000

    fix: Fixed issue where navigation was not using owner-provided URLs

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -11,11 +11,18 @@ import { Logo } from "components/Logo";
 
 import { navigationSelector } from "./selector";
 import { NavigationProps } from "./types";
+import { DefaultDocsAndSupport } from "./constants";
 
 export const Navigation: React.FC<NavigationProps> = ({ contractible = false, className, ...rest }) => {
   const dispatch = useDispatch();
   const { palette, zIndex, spacing } = useTheme();
-  const { navigation, portalName, ownerInfo } = useConfig();
+  const { navigation, portalName, ownerInfo, documentationURL, supportURL } = useConfig();
+
+  console.log("navigation:", navigation);
+  console.log("ownerInfo:", ownerInfo);
+  console.log("documentationURL:", documentationURL);
+  console.log("supportURL:", supportURL);
+
   const { t } = useTranslation();
   const { user, currentOrg } = useSelector(navigationSelector);
   // FIXME: checking the id because profile is never undefined
@@ -136,12 +143,31 @@ export const Navigation: React.FC<NavigationProps> = ({ contractible = false, cl
         style={adjustTop ? { transform: "translateY(-2px)" } : undefined}
         color={!expand && subTab ? palette.text.primary : palette.secondary.contrastText}
       >
+        {/* Handling of 'Documentation' and 'Support' tabs, which might point to external links */}
+        {
+          action === DefaultDocsAndSupport.documentation &&
+            <Link to={documentationURL || DefaultDocsAndSupport.documentation} style={{ textDecoration: "none" }}>
+              {LabelComponent}
+            </Link>
+        }
+
+        {
+          action === DefaultDocsAndSupport.support &&
+            <Link to={supportURL || DefaultDocsAndSupport.support} style={{ textDecoration: "none" }}>
+              {LabelComponent}
+            </Link>
+        }
+
         {/* routing actions - starts with `/` or `http` */}
-        {/^(\/|http)/.test(action) && (
-          <Link to={action} style={{ textDecoration: "none" }}>
-            {LabelComponent}
-          </Link>
-        )}
+        {
+          /^(\/|http)/.test(action) &&
+          action !== DefaultDocsAndSupport.documentation &&
+          action !== DefaultDocsAndSupport.support && (
+            <Link to={action} style={{ textDecoration: "none" }}>
+              {LabelComponent}
+            </Link>
+          )
+        }
 
         {/* hook actions - first condition is to be taken as explicit */}
         {action.startsWith("$") && actions.hasOwnProperty(action) && (
@@ -232,6 +258,7 @@ export const Navigation: React.FC<NavigationProps> = ({ contractible = false, cl
           justify="flex-end"
         >
           {navigation[role].tabs.map((tab) => {
+            console.log("tab:", tab);
             if (expand && !tab.fixed) return null;
 
             return renderTab(tab);
