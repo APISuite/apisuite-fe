9054ce748729130ecf6b64d95c12b5ad482da295
pedroCloudoki <67902809+pedroCloudoki@users.noreply.github.com>
2022-02-21 09:21:19 +0000

    fix: Fixed issue where navigation was not using owner-provided URLs (#559)

--- src/components/Navigation/constants.ts
+++ src/components/Navigation/constants.ts
@@ -3,3 +3,8 @@ export enum NavigationLeftActionTypes {
   goBack = "goBack",
   openCard = "openCard",
 }
+
+export enum DefaultDocsAndSupport {
+  documentation = "/documentation",
+  support = "https://intercom.help/api-suite/en",
+}
