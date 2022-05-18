59d7870f50373102ad30439e3049ceaaf0dc6855
jcastilho <joaocastilho@cloudoki.com>
2022-05-18 10:51:23 +0100

    fix: circleci expand build timeout

--- extensions.ts
+++ extensions.ts
@@ -1,11 +1,13 @@
 import { Extension as ExtensionV1 } from "@apisuite/extension-ui-types/v1";
-// import { instanceExtension } from 'util/extensionsParams'
+import { instanceExtension } from 'util/extensionsParams'
 // import Example from '@apisuite/extension-ui-example'
+//import Marketplace from "@apisuite/apisuite-marketplace-extension-ui";
 
 type RegistryEntry = ExtensionV1[]
 
 const registry: RegistryEntry = [
   // instanceExtension(Example, { someKey: 'Overriden value' }),
+  //instanceExtension(Marketplace, {}),
 ].filter(Boolean);
 
 export default registry;
