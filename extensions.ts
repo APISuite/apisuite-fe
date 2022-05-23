b3e48b627bb8e3276716dc9253289b07048e3075
Luis Troni <luis.troni@gmail.com>
2022-05-23 11:46:52 +0100

    fix: fixes extensions

--- extensions.ts
+++ extensions.ts
@@ -1,17 +1,14 @@
 import { Extension as ExtensionV1 } from "@apisuite/extension-ui-types/v1";
-import { instanceExtension } from "util/extensionsParams";
-import Admin from "@apisuite/extension-cloud-extension-ui";
-import Billing from "@apisuite/apisuite-billing-extension-ui";
-import Marketplace from "@apisuite/apisuite-marketplace-extension-ui";
+//import { instanceExtension } from 'util/extensionsParams'
+//import { instanceExtension } from 'util/extensionsParams'
+// import Example from '@apisuite/extension-ui-example'
+//import Marketplace from "@apisuite/apisuite-marketplace-extension-ui";
 
 type RegistryEntry = ExtensionV1[]
 
 const registry: RegistryEntry = [
-  instanceExtension(Admin, {
-    cloudExtensionUrl: "https://cloudextapi.apisuite.io",
-  }),
-  instanceExtension(Billing, {}),
-  instanceExtension(Marketplace, {}),
+  // instanceExtension(Example, { someKey: 'Overriden value' }),
+  //instanceExtension(Marketplace, {}),
 ].filter(Boolean);
 
 export default registry;
