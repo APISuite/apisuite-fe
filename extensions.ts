dfad99167649e05474dc6054c58a389dd154fa83
jcastilho <joaocastilho@cloudoki.com>
2022-06-23 13:18:23 +0100

    feat: solo deploy

--- extensions.ts
+++ extensions.ts
@@ -1,14 +1,17 @@
 import { Extension as ExtensionV1 } from "@apisuite/extension-ui-types/v1";
 //import { instanceExtension } from 'util/extensionsParams'
-//import { instanceExtension } from 'util/extensionsParams'
-// import Example from '@apisuite/extension-ui-example'
-//import Marketplace from "@apisuite/apisuite-marketplace-extension-ui";
+import { instanceExtension } from "util/extensionsParams";
+import Admin from "@apisuite/extension-cloud-extension-ui";
+import Marketplace from "@apisuite/apisuite-marketplace-extension-ui";
 
 type RegistryEntry = ExtensionV1[]
 
 const registry: RegistryEntry = [
-  // instanceExtension(Example, { someKey: 'Overriden value' }),
-  //instanceExtension(Marketplace, {}),
+  instanceExtension(Admin, {
+    cloudExtensionUrl: "https://cloudextapi.apisuite.io",
+  }),
+  instanceExtension(Billing, {}),
+  instanceExtension(Marketplace, {}),
 ].filter(Boolean);
 
 export default registry;
